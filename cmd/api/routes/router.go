70e9891297f00a705d7db3748c98f0fe27fe19cd
Jason Hilder <jhilder95@gmail.com>
2021-01-02 20:44:46 +0200

    updated router.

--- cmd/api/routes/router.go
+++ cmd/api/routes/router.go
@@ -1,23 +1,29 @@
 package router
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	// Apllication dependancy
+	"github.com/JSONhilder/overseer_api/internal/application"
+
+	// Handlers
+	"github.com/JSONhilder/overseer_api/cmd/api/handlers/projects"
+	"github.com/JSONhilder/overseer_api/cmd/api/handlers/server"
 
 	// import libraries
 	"github.com/gorilla/mux"
 )
 
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Server is running.")
-}
-
 // StartRouter - initials the routers with their respective handlers
-func StartRouter() {
+func StartRouter(app *application.Application) {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/", healthCheck)
+	myRouter.HandleFunc("/", server.Check(app))
+	myRouter.HandleFunc("/api/projects", projects.CreateProject(app)).Methods("POST")
+	myRouter.HandleFunc("/api/projects/{id}", projects.GetProject(app)).Methods("GET")
+	myRouter.HandleFunc("/api/projects", projects.GetProjects(app))
 
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
+	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), myRouter))
 }
