f1bca752aa1edfb3aa46741056d944540e8ba02b
Jason Hilder <jhilder95@gmail.com>
2021-01-02 20:45:41 +0200

    application dependancy.

---
+++ internal/application/application.go
@@ -0,0 +1,36 @@
+package application
+
+import (
+	"github.com/JSONhilder/overseer_api/internal/config"
+	"github.com/JSONhilder/overseer_api/internal/db"
+	"go.uber.org/zap"
+)
+
+// Application - struct with pointers to db instance and config object
+type Application struct {
+	DB  *db.DB
+	CFG *config.Config
+	LOG *zap.SugaredLogger
+}
+
+// Get - Return an application state with access to db, config and logger for dependancy injection
+func Get() (*Application, error) {
+	cfg := config.Get()
+
+	// Init the logger and add to apllication struct
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync() // flushes buffer, if any
+	log := logger.Sugar()
+
+	db, err := db.Get(cfg)
+	if err != nil {
+		return nil, err
+	}
+	log.Info("Database successfully connected.")
+
+	return &Application{
+		DB:  db,
+		CFG: cfg,
+		LOG: log,
+	}, nil
+}
