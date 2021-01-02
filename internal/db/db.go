455dd9a232e09a09db1f77ec6866a148e66c5be0
Jason Hilder <jhilder95@gmail.com>
2021-01-02 20:45:02 +0200

    db methods, setup and init.

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,36 +3,74 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/JSONhilder/overseer_api/internal/config"
 
 	// postgres sql driver
 	_ "github.com/lib/pq"
 )
 
-const (
-	// @todo - move to env file
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "root"
-	dbname   = "overseer_main"
-)
+// DB - DB struct to hold instance of db to pass around app
+type DB struct {
+	Client *sql.DB
+}
 
-// ConnectDB - Create db connection function
-func ConnectDB() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
+// Get - get db instance
+func Get(cfg *config.Config) (*DB, error) {
 	// opening connection to db
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := initDB(cfg)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	defer db.Close()
+	//defer db.Close()
+
+	return &DB{
+		Client: db,
+	}, nil
+}
+
+func initDB(cfg *config.Config) (*sql.DB, error) {
+	psqlInfo := config.GetDBConnString(cfg)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Successfully connected!")
 
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Successfully connected!")
+	setupTables(db)
+
+	return db, nil
+}
+
+// Close - Helper function to close instance of db
+func (d *DB) Close() error {
+	return d.Client.Close()
+}
+
+// Setup tables
+func setupTables(db *sql.DB) {
+
+	query, err := ioutil.ReadFile("/home/jason/dev/github/overseer_api/internal/db/setup_db.sql")
+	if err != nil {
+		panic(err)
+	}
+
+	requests := strings.Split(string(query), ";")
+
+	for _, request := range requests {
+		_, err := db.Exec(request)
+
+		if err != nil {
+			panic(err)
+		}
+	}
 }
