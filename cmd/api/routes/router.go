2e2b3eebe4da829bb2d6860baaf86a9ae5c3bc02
Jason Hilder <jhilder95@gmail.com>
2021-01-07 10:28:18 +0200

    Added tasks endpoints and middleware.

--- cmd/api/routes/router.go
+++ cmd/api/routes/router.go
@@ -7,11 +7,12 @@ import (
 
 	// Apllication dependancy
 	"github.com/JSONhilder/overseer_api/internal/application"
-	"github.com/JSONhilder/overseer_api/internal/middleware/authjwt"
+	"github.com/JSONhilder/overseer_api/internal/middleware"
 
 	// Handlers
 	"github.com/JSONhilder/overseer_api/cmd/api/handlers/projects"
 	"github.com/JSONhilder/overseer_api/cmd/api/handlers/server"
+	"github.com/JSONhilder/overseer_api/cmd/api/handlers/tasks"
 
 	// import libraries
 	"github.com/gorilla/mux"
@@ -23,12 +24,17 @@ func StartRouter(app *application.Application) {
 	router.HandleFunc("/", server.Check(app))
 
 	authRoutes := router.PathPrefix("/api").Subrouter()
-	authRoutes.Use(authjwt.Verify)
+	authRoutes.Use(middleware.VerifyJwt)
 	authRoutes.HandleFunc("/projects", projects.CreateProject(app)).Methods("POST")
-	authRoutes.HandleFunc("/projects/{id}", projects.GetProject(app)).Methods("GET")
 	authRoutes.HandleFunc("/projects/{id}", projects.UpdateProject(app)).Methods("PUT")
 	authRoutes.HandleFunc("/projects/{id}", projects.DeleteProject(app)).Methods("DELETE")
+	authRoutes.HandleFunc("/projects/{id}", projects.GetProject(app))
 	authRoutes.HandleFunc("/projects", projects.GetProjects(app))
+
+	authRoutes.HandleFunc("/tasks", tasks.CreateTask(app)).Methods("POST")
+	authRoutes.HandleFunc("/tasks/{id}", tasks.DeleteTask(app)).Methods("DELETE")
+	authRoutes.HandleFunc("/tasks/{id}", tasks.UpdateTask(app)).Methods("PUT")
+	authRoutes.HandleFunc("/tasks", tasks.GetTasks(app))
 
 	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), router))
 }
